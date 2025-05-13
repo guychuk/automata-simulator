d5b063ad137f0a8446dee6cbd635ee42b8b3e4ca
Guy Moalem <guy.moa@gmail.com>
2025-05-14 00:41:54 +0300

    build and pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {  
-    mode: 'development',
-    devtool: 'eval-source-map', // for production: 'source-map'
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,  // '$' means the end of the file
-                use: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')],  // __dirname is the current directory
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        publicPath: '',
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'public')
-    }
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    publicPath: "",
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
+    }),
+  ],
 };
